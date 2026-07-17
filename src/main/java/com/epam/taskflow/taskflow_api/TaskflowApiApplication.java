ac7bd4b986a76d1de20725192742b78fb360a03b
yashkumar-epam <patelyashkumar.laljibhai@epam.com>
2026-07-17 15:17:11 +0530

    Day 1: Initial Spring Boot project setup - TaskFlow API

---
+++ src/main/java/com/epam/taskflow/taskflow_api/TaskflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.epam.taskflow.taskflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/taskflow/taskflow_api/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.taskflow.taskflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
